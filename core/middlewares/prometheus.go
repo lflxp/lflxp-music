777d903cc9c8aa1555fa15d06f2379a1a73e8bc2
lflxp <382023823@qq.com>
2023-02-18 02:19:45 +0800

    remove something

--- core/middlewares/prometheus.go
+++ core/middlewares/prometheus.go
@@ -2,47 +2,12 @@
 package middlewares
 
 import (
-	"fmt"
-	"math"
-	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-
-var (
-	// Create a summary to track fictional interservice RPC latencies for three
-	// distinct services with different latency distributions. These services are
-	// differentiated via a "service" label.
-	rpcDurations = prometheus.NewSummaryVec(
-		prometheus.SummaryOpts{
-			Name:       "rpc_durations_seconds",
-			Help:       "RPC latency distributions.",
-			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
-		},
-		[]string{"service"},
-	)
-	// The same as above, but now as a histogram, and only for the normal
-	// distribution. The buckets are targeted to the parameters of the
-	// normal distribution, with 20 buckets centered on the mean, each
-	// half-sigma wide.
-	rpcDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "rpc_durations_histogram_seconds",
-		Help:    "RPC latency distributions.",
-		Buckets: prometheus.LinearBuckets(0.00001-5*0.0002, .5*0.0002, 20),
-	})
-)
-
-func init() {
-	// Register the summary and the histogram with Prometheus's default registry.
-	prometheus.MustRegister(rpcDurations)
-	prometheus.MustRegister(rpcDurationsHistogram)
-	// Add Go module build info.
-	prometheus.MustRegister(prometheus.NewBuildInfoCollector())
-}
 
 // 默认prometheus基础监控
 // func RegisterPrometheusMiddleware(router *gin.Engine, isauth bool) {
@@ -67,46 +32,6 @@ func PromHandler(handler http.Handler) gin.HandlerFunc {
 
 // 默认prometheus监控+自定义监控
 func RegisterPrometheusMiddleware(router *gin.Engine, isauth bool) {
-	start := time.Now()
-
-	oscillationFactor := func() float64 {
-		return 2 + math.Sin(math.Sin(2*math.Pi*float64(time.Since(start))/float64(10*time.Minute)))
-	}
-
-	// Periodically record some sample latencies for the three services.
-	go func() {
-		for {
-			v := rand.Float64() * 0.0002
-			rpcDurations.WithLabelValues("uniform").Observe(v)
-			time.Sleep(time.Duration(100*oscillationFactor()) * time.Millisecond)
-		}
-	}()
-
-	go func() {
-		for {
-			v := (rand.NormFloat64() * 0.0002) + 0.00001
-			rpcDurations.WithLabelValues("normal").Observe(v)
-			// Demonstrate exemplar support with a dummy ID. This
-			// would be something like a trace ID in a real
-			// application.  Note the necessary type assertion. We
-			// already know that rpcDurationsHistogram implements
-			// the ExemplarObserver interface and thus don't need to
-			// check the outcome of the type assertion.
-			rpcDurationsHistogram.(prometheus.ExemplarObserver).ObserveWithExemplar(
-				v, prometheus.Labels{"dummyID": fmt.Sprint(rand.Intn(100000))},
-			)
-			time.Sleep(time.Duration(75*oscillationFactor()) * time.Millisecond)
-		}
-	}()
-
-	go func() {
-		for {
-			v := rand.ExpFloat64() / 1e6
-			rpcDurations.WithLabelValues("exponential").Observe(v)
-			time.Sleep(time.Duration(50*oscillationFactor()) * time.Millisecond)
-		}
-	}()
-
 	if isauth {
 		group := router.Group("/metrics", gin.BasicAuth(gin.Accounts{
 			"root": "system",
